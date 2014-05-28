da6a396b9fabb5a1c8485ce454fdbacd8fdb788e
claudia-romano <claudia.romano06@gmail.com>
2014-05-28 11:23:59 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-side-shopping-cart
-==================
+Side Shopping Cart
+=========
 
-Animated side shopping cart
+An animated side cart to let users jump in and out from the list of products they want to buy, without having to refresh the page or fire a popup.
+
+[Article on Cody](http://codyhouse.co/gem/side-shopping-cart/)
+
+[Demo](http://codyhouse.co/demo/side-cart/)
+ 
+[Terms](http://codyhouse.co/terms/)
+
+
+[© Cody 2014](http://codyhouse.co/)
---
+++ js/main.js
@@ -0,0 +1,67 @@
+jQuery(document).ready(function($){
+	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+	var $L = 1200,
+		$menu_navigation = $('#main-nav'),
+		$cart_trigger = $('#cd-cart-trigger'),
+		$hamburger_icon = $('#cd-hamburger-menu'),
+		$lateral_cart = $('#cd-cart'),
+		$shadow_layer = $('#cd-shadow-layer');
+
+	//open lateral menu on mobile
+	$hamburger_icon.on('click', function(event){
+		event.preventDefault();
+		//close cart panel (if it's open)
+		$lateral_cart.removeClass('speed-in');
+		toggle_panel_visibility($menu_navigation, $shadow_layer);
+	});
+
+	//open cart
+	$cart_trigger.on('click', function(event){
+		event.preventDefault();
+		//close lateral menu (if it's open)
+		$menu_navigation.removeClass('speed-in');
+		toggle_panel_visibility($lateral_cart, $shadow_layer);
+	});
+
+	//close lateral cart or lateral menu
+	$shadow_layer.on('click', function(){
+		$lateral_cart.removeClass('speed-in');
+		$menu_navigation.removeClass('speed-in');
+		$shadow_layer.removeClass('is-visible');
+	});
+
+	//move #main-navigation inside header on laptop
+	//insert #main-navigation after header on mobile
+	move_navigation( $menu_navigation, $L);
+	$(window).on('resize', function(){
+		move_navigation( $menu_navigation, $L);
+		
+		if( $(window).width() >= $L && $menu_navigation.hasClass('speed-in')) {
+			$menu_navigation.removeClass('speed-in');
+			$shadow_layer.removeClass('is-visible');
+		}
+
+	});
+});
+
+function toggle_panel_visibility ($lateral_panel, $background_layer) {
+	if( $lateral_panel.hasClass('speed-in') ) {
+		// $trigger.removeClass('is-clicked');
+		$lateral_panel.removeClass('speed-in');
+		$background_layer.removeClass('is-visible');
+	} else {
+		// $trigger.addClass('is-clicked');
+		$lateral_panel.addClass('speed-in');
+		$background_layer.addClass('is-visible');
+	}
+}
+
+function move_navigation( $navigation, $MQ) {
+	if ( $(window).width() >= $MQ ) {
+		$navigation.detach();
+		$navigation.appendTo('header');
+	} else {
+		$navigation.detach();
+		$navigation.insertAfter('header');
+	}
+}
